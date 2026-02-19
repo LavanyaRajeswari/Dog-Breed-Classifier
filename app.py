fbf13d63139f5ef9a246d133d13ccd328025ba8f
Lavanya Rajeswari <saridelavanyarajeswari@gmail.com>
2026-02-19 14:18:00 +0530

    app.py update

--- app.py
+++ app.py
@@ -9,29 +9,24 @@ import os
 app = Flask(__name__)
 CORS(app)
 
-MODEL_PATH = "dog_breed_model.h5"
-CLASS_INDEX_PATH = "class_indices.json"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+MODEL_PATH = os.path.join(BASE_DIR, "dog_breed_model.h5")
+CLASS_INDEX_PATH = os.path.join(BASE_DIR, "class_indices.json")
+
 IMG_SIZE = 224
 
-# Load model
 model = None
 
 def load_model_once():
     global model
     if model is None:
-        print("Loading model...")
         model = tf.keras.models.load_model(MODEL_PATH)
-        print("Model loaded successfully!")
 
-
-# Load class indices
 with open(CLASS_INDEX_PATH, "r") as f:
     class_indices = json.load(f)
 
-# Reverse dictionary (index → breed)
 index_to_class = {v: k for k, v in class_indices.items()}
-
-print("Total Breeds:", len(index_to_class))
 
 def preprocess_image(image):
     image = image.resize((IMG_SIZE, IMG_SIZE))
@@ -39,7 +34,14 @@ def preprocess_image(image):
     image = np.expand_dims(image, axis=0)
     return image
 
-@app.route("/", methods=["POST"])
+@app.route("/", methods=["GET"])
+def home():
+    return jsonify({
+        "message": "Dog Breed Classifier API is Running",
+        "endpoint": "/predict (POST)"
+    })
+
+@app.route("/predict", methods=["POST"])
 def predict():
     load_model_once()
 
@@ -53,15 +55,12 @@ def predict():
         image = preprocess_image(image)
 
         prediction = model.predict(image)[0]
-
-        # Top 3 predictions
         top_3_indices = prediction.argsort()[-3:][::-1]
 
         results = []
         for idx in top_3_indices:
             breed_name = index_to_class[idx].replace("_", " ").title()
             confidence = float(round(prediction[idx] * 100, 2))
-
             results.append({
                 "breed": breed_name,
                 "confidence": confidence
